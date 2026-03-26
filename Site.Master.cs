c39677750e7bf0e448c7c72117e395fb59cd7af6
unknown <ravindra21432@gmail.com>
2026-03-26 09:30:01 +0530

    master dropdown

--- Site.Master.cs
+++ Site.Master.cs
@@ -31,65 +31,41 @@ namespace Aptum
                 StringBuilder desktopNav = new StringBuilder();
                 StringBuilder mobileNav = new StringBuilder();
 
-                string currentUrl = HttpContext.Current.Request.Url.AbsolutePath.ToLower();
-
                 DataRow[] parents = dt.Select("ParentID IS NULL OR ParentID = 0");
 
                 foreach (DataRow row in parents)
                 {
                     string id = row["ID"].ToString();
-
-                    // 🔥 FIX: Text break issue (About Us ek line me rahe)
                     string title = row["Title"].ToString().Replace(" ", "&nbsp;");
-
                     string navUrl = row["Navurl"].ToString();
 
                     DataRow[] children = dt.Select("ParentID = " + id);
-
-                    string isActive = currentUrl.Contains(navUrl.ToLower())
-                        ? "text-orange-500 font-bold"
-                        : "text-black";
 
                     // ✅ SIMPLE MENU
                     if (children.Length == 0)
                     {
                         desktopNav.Append($@"
                             <li>
-                                <a href='/{navUrl}' 
-                                   class='px-4 py-2 text-[15px] font-semibold whitespace-nowrap {isActive} hover:text-orange-500 transition duration-200'>
-                                    {title}
-                                </a>
-                            </li>
-                        ");
-
-                        mobileNav.Append($@"
-                            <li>
-                                <a href='/{navUrl}' class='block py-2 text-gray-800 hover:text-orange-500'>
-                                    {title.Replace("&nbsp;", " ")}
-                                </a>
-                            </li>
-                        ");
+                            <a href='/{navUrl}' 
+                            class='px-4 py-2 text-[15px] font-semibold whitespace-nowrap text-black hover:text-orange-500 transition duration-200'>
+                          {title}
+                              </a>
+                           </li>"
+                        );
                     }
                     else
                     {
-                        // ✅ DROPDOWN MENU
-                        desktopNav.Append($@"
-                            <li class='relative group'>
-                                <button class='flex items-center gap-1 px-4 py-2 text-[15px] font-semibold whitespace-nowrap {isActive} hover:text-orange-500 transition duration-200'>
-                                    {title} <i class='fas fa-chevron-down text-xs'></i>
-                                </button>
+                        // 🔥 MEGA MENU
+                        desktopNav.Append($@"<li class='group'>
+  <button class='flex items-center gap-1 px-4 py-2 text-[15px] font-semibold text-black hover:text-orange-500'>
+  {title} <i class='fas fa-chevron-down text-xs'></i>
+  </button>
 
-                                <ul class='absolute hidden group-hover:block bg-white text-gray-800 mt-2 rounded-lg shadow-xl min-w-[200px] z-50'>
-                        ");
+  <!-- FULL WIDTH MEGA MENU -->
+  <div class='absolute left-1/2 -translate-x-1/2 top-full w-screen hidden group-hover:block bg-white shadow-xl z-50'>
 
-                        mobileNav.Append($@"
-                            <li>
-                                <button onclick=""this.nextElementSibling.classList.toggle('hidden')"" 
-                                        class='w-full text-left py-2 font-medium'>
-                                    {title.Replace("&nbsp;", " ")}
-                                </button>
-                                <ul class='hidden pl-4'>
-                        ");
+    <div class='max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-10 gap-y-4 px-10 py-6'>
+");
 
                         foreach (DataRow child in children)
                         {
@@ -97,35 +73,35 @@ namespace Aptum
                             string childUrl = child["Navurl"].ToString();
 
                             desktopNav.Append($@"
-                                <li>
-                                    <a href='/{childUrl}' 
-                                       class='block px-4 py-2 hover:bg-orange-50 hover:text-orange-500 transition'>
-                                        {childTitle}
-                                    </a>
-                                </li>
-                            ");
+                         <div>
+                              <a href='/{childUrl}' 
+                             class='flex items-start gap-3 p-3 rounded hover:bg-orange-50 hover:text-orange-500 transition'>
 
-                            mobileNav.Append($@"
-                                <li>
-                                    <a href='/{childUrl}' class='block py-1 text-gray-700 hover:text-orange-500'>
-                                        {childTitle}
-                                    </a>
-                                </li>
-                            ");
+                              <i class='fas fa-code text-orange-500 mt-1'></i>
+
+                                 <span class='text-sm font-medium text-gray-800 leading-tight'>
+                                {childTitle}
+                                   </span>
+
+                               </a>
+                             </div>"
+                            );
                         }
 
-                        desktopNav.Append("</ul></li>");
-                        mobileNav.Append("</ul></li>");
+                        desktopNav.Append(@"
+                                </div>
+                                    </div>
+                          </li>"
+                        );
                     }
                 }
 
                 _LiteralNavDesktop.Text = desktopNav.ToString();
-                _LiteralNavMobile.Text = mobileNav.ToString();
+                _LiteralNavMobile.Text = "";
             }
             catch (Exception)
             {
                 _LiteralNavDesktop.Text = "<li>Error loading menu</li>";
-                _LiteralNavMobile.Text = "<li>Error loading menu</li>";
             }
         }
     }
